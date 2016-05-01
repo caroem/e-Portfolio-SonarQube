f4e88e96e3ae3626707040692dccf90e591a84e4
caromehret <chi340@hotmail.com>
2016-05-02 00:56:21 +0200

    Added Samples

---
+++ Java with Gradle Sample/SQ25/src/src/DFA.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.util.logging.Logger;
+
+/**
+ * Created by caroem on 17.04.16.
+ * Todo: Create createState Method
+ */
+public class DFA {
+    private List<State> states = new ArrayList<>();
+    private List<Transition> transition = new ArrayList<>();
+    private char inputToCharArray[];
+    private State currentState;
+    private State errorState;
+    private List<State> finalStates = new ArrayList<>();
+    private List<Character> alphabet = new ArrayList<>();
+    private Logger logger;
+
+    public void addState(int state, boolean isFinal, boolean isInitial) {
+        State newState = new State(state, isFinal, isInitial);
+        states.add(newState);
+        if(isFinal) {
+            finalStates.add(newState);
+        }
+    }
+
+    public void addTransition(int source, int destination, char input) {
+        Transition newTransition = new Transition(getStateForTransition(source), getStateForTransition(destination), input);
+        transition.add(newTransition);
+        alphabet.add(input);
+    }
+
+    public State getStateForTransition(int state) {
+        for(int i = 0; i < states.size(); i++) {
+            if(states.get(i).state == state) {
+                return states.get(i);
+            }
+        }
+        return null;
+    }
+
+    public boolean readCharacters(char input) {
+        for(int i = 0; i < transition.size(); i++) {
+            if((transition.get(i).source == currentState) && (transition.get(i).input == input)) {
+                currentState = transition.get(i).destination;
+                return true;
+            }
+        }
+        if(!alphabet.contains(input)) {
+            currentState = errorState;
+            return false;
+        }
+        currentState = errorState;
+        return false;
+    }
+
+    public boolean acceptInput(String input) {
+        inputToCharArray = new char[input.length()];
+        inputToCharArray = input.toCharArray();
+        for (int i = 0; i < inputToCharArray.length; i++) {
+            readCharacters(inputToCharArray[i]);
+        }
+
+        if(finalStates.contains(currentState)) {
+            System.out.println("Accepted!");
+            setInitialState();
+            return true;
+        } else {
+            System.out.println("Not Accepted!");
+            setInitialState();
+            return false;
+        }
+    }
+
+    public void setInitialState() {
+        for (int i = 0; i < states.size(); i++) {
+            if (states.get(i).isInitial) {
+                currentState = states.get(i);
+            }
+        }
+    }
+
+    public void setErrorState(int state) {
+        for(int i = 0; i < states.size(); i++) {
+            if(states.get(i).state == state) {
+                errorState = states.get(i);
+            }
+        }
+    }
+
+
+}
---
+++ Java with Gradle Sample/SQ25/src/src/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Created by caroem on 22.04.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // The following states and transitions create a DFA which accepts floating point numbers
+        // numbers from 0-9 are represented by n
+        DFA dea = new DFA();
+        dea.addState(0, false, true);
+        dea.addState(1, true, false);
+        dea.addState(2, false, false);
+        dea.addState(3, false, false);
+        dea.addState(4, true, false);
+        dea.addState(5, true, false);
+        dea.addState(6, false, false);
+        dea.addState(-1, false, false);
+        dea.setInitialState();
+        dea.setErrorState(-1);
+        dea.addTransition(0, 1, 'n');
+        dea.addTransition(0, 2, '.');
+        dea.addTransition(1, 2, '.');
+        dea.addTransition(1, 3, 'e');
+        dea.addTransition(2, 4, 'n');
+        dea.addTransition(3, 5, '.');
+        dea.addTransition(3, 6, '+');
+        dea.addTransition(3, 6, '-');
+        dea.addTransition(4, 3, 'e');
+        dea.addTransition(6, 5, 'n');
+        dea.acceptInput("n.n");
+    }
+}
